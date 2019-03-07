4f916a3569558b2653c3b436f65efd1be5a2007d
Eridan123 <saryguloveridan@gmail.com>
2019-03-08 05:47:34 +0600

    template engine resolver config added

---
+++ src/main/java/law/advisor/config/AppConfig.java
@@ -0,0 +1,93 @@
+package law.advisor.config;
+
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.ApplicationContextAware;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.support.ReloadableResourceBundleMessageSource;
+import org.springframework.http.CacheControl;
+import org.springframework.web.multipart.MultipartResolver;
+import org.springframework.web.multipart.support.StandardServletMultipartResolver;
+import org.springframework.web.servlet.ViewResolver;
+import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
+import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter;
+import org.springframework.web.servlet.i18n.CookieLocaleResolver;
+import org.springframework.web.servlet.i18n.LocaleChangeInterceptor;
+import org.thymeleaf.TemplateEngine;
+import org.thymeleaf.dialect.IDialect;
+import org.thymeleaf.spring5.ISpringTemplateEngine;
+import org.thymeleaf.spring5.SpringTemplateEngine;
+import org.thymeleaf.spring5.dialect.SpringStandardDialect;
+import org.thymeleaf.spring5.templateresolver.SpringResourceTemplateResolver;
+import org.thymeleaf.spring5.view.ThymeleafViewResolver;
+import org.thymeleaf.templatemode.TemplateMode;
+import org.thymeleaf.templateresolver.ITemplateResolver;
+
+import java.util.HashSet;
+import java.util.Locale;
+import java.util.Set;
+
+@Configuration
+public class AppConfig extends WebMvcConfigurerAdapter implements ApplicationContextAware {
+
+    private static final String UTF8 = "UTF-8";
+
+    private ApplicationContext applicationContext;
+
+    @Override
+    public void setApplicationContext(ApplicationContext applicationContext) {
+        this.applicationContext = applicationContext;
+    }
+
+    @Bean
+    public ViewResolver viewResolver() {
+        ThymeleafViewResolver resolver = new ThymeleafViewResolver();
+        resolver.setTemplateEngine((ISpringTemplateEngine) templateEngine());
+        resolver.setCharacterEncoding(UTF8);
+        resolver.setContentType("text/html;charset=UTF-8");
+        resolver.setCache(false);
+        return resolver;
+    }
+
+    private TemplateEngine templateEngine() {
+        SpringTemplateEngine engine = new SpringTemplateEngine();
+
+        Set<IDialect> dialects = new HashSet<>();
+        dialects.add(new SpringStandardDialect());
+
+
+        engine.setTemplateResolver(templateResolver());
+//        engine.setMessageSource(getMessageSource());
+
+        engine.setAdditionalDialects(dialects);
+        return engine;
+    }
+
+    private ITemplateResolver templateResolver() {
+        SpringResourceTemplateResolver resolver = new SpringResourceTemplateResolver();
+        resolver.setApplicationContext(applicationContext);
+        resolver.setPrefix("/resources/templates/");
+        resolver.setSuffix(".html");
+        resolver.setCharacterEncoding(UTF8);
+        resolver.setTemplateMode(TemplateMode.HTML);
+        resolver.setCacheable(false);
+        return resolver;
+    }
+
+
+    @Bean(name = "multipartResolver")
+    public MultipartResolver createMultipartResolver() {
+        return new StandardServletMultipartResolver();
+    }
+
+//    @Override
+//    public void addResourceHandlers(ResourceHandlerRegistry registry) {
+//        registry.addResourceHandler("/static/**").addResourceLocations("/static/").setCacheControl(CacheControl.maxAge(365, TimeUnit.DAYS));
+//        registry.addResourceHandler("/assets/**").addResourceLocations("/static/assets/").setCacheControl(CacheControl.maxAge(365, TimeUnit.DAYS));
+//        registry.addResourceHandler("/js/**").addResourceLocations("/static/js/").setCacheControl(CacheControl.maxAge(1, TimeUnit.DAYS));
+//        registry.addResourceHandler("/css/**").addResourceLocations("/static/css/").setCacheControl(CacheControl.maxAge(365, TimeUnit.DAYS));
+//        registry.addResourceHandler("/resources/**").addResourceLocations("/resources/").setCacheControl(CacheControl.maxAge(365, TimeUnit.DAYS));
+//    }
+
+}
