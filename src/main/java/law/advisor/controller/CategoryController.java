0584b466083098e0ac70293ffd8101e0b21e4389
aba-bakri <36129976+aba-bakri@users.noreply.github.com>
2019-03-12 10:11:31 +0600

    Update CategoryController.java

--- src/main/java/law/advisor/controller/CategoryController.java
+++ src/main/java/law/advisor/controller/CategoryController.java
@@ -10,6 +10,7 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 
 import java.util.List;
+
 @Controller
 public class CategoryController {
 
