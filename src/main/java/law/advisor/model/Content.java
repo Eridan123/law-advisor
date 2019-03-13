96832a3dfc2eaf92a013e8d41ab2e07937297f86
kadyrov123 <n.kadyrov123@gmail.com>
2019-03-13 21:19:23 +0600

    Merge branch 'master' of /home/nurik/Desktop/law-advisor with conflicts.

--- src/main/java/law/advisor/model/Content.java
+++ src/main/java/law/advisor/model/Content.java
@@ -12,4 +12,20 @@ public class Content {
 
     @Column(columnDefinition = "TEXT")
     private String text;
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getText() {
+        return text;
+    }
+
+    public void setText(String text) {
+        this.text = text;
+    }
 }
--- src/main/java/law/advisor/model/Question.java
+++ src/main/java/law/advisor/model/Question.java
@@ -7,6 +7,54 @@ import java.util.Date;
 @Table(name = "question")
 public class Question {
 
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public Date getDate() {
+        return date;
+    }
+
+    public void setDate(Date date) {
+        this.date = date;
+    }
+
+    public int getStatus() {
+        return status;
+    }
+
+    public void setStatus(int status) {
+        this.status = status;
+    }
+
+    public Category getCategory() {
+        return category;
+    }
+
+    public void setCategory(Category category) {
+        this.category = category;
+    }
+
+    public User getUser() {
+        return user;
+    }
+
+    public void setUser(User user) {
+        this.user = user;
+    }
+
     @Id
     @GeneratedValue(strategy = GenerationType.AUTO)
     private Long id;
