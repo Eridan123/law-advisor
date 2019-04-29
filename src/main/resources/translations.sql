0fabab2eca203b5d34d2f4e0092bd95dfa8e0b58
Eridan123 <saryguloveridan@gmail.com>
2019-04-29 06:08:39 +0600

    translations added

---
+++ src/main/resources/translations.sql
@@ -0,0 +1,69 @@
+insert into message_resource(kgz,rus,eng,messageKey) values ('ОНЛАЙН ЮРИДИКАЛЫК ЖАРДАМЧЫ','ОНЛАЙН ЮРИДИЧЕСКИЙ КОНСУЛЬТАНТ','ONLINE LAW CONSULTANT','label.title');
+insert into message_resource(kgz,rus,eng,messageKey) values ('ОРУС','РУ','RU','label.ru');
+insert into message_resource(kgz,rus,eng,messageKey) values ('АНГЛ','АНГЛ','EN','label.en');
+insert into message_resource(kgz,rus,eng,messageKey) values ('КЫРГ','КЫРГ','KG','label.kg');
+insert into message_resource(kgz,rus,eng,messageKey) values ('БАШКЫ БЕТ','ГЛАВНАЯ','HOME','label.home');
+insert into message_resource(kgz,rus,eng,messageKey) values ('ЖАНЫЛЫКТАР','НОВОСТИ','NEWS','label.news');
+insert into message_resource(kgz,rus,eng,messageKey) values ('БИЗ ЖОНУНДО','О НАС','ABOUT US','label.aboutus');
+insert into message_resource(kgz,rus,eng,messageKey) values ('','ОТЗЫВЫ','REVIEWS','label.reviews');
+insert into message_resource(kgz,rus,eng,messageKey) values ('ТЕХ. КОЛДОО','ТЕХ. ПОДДЕРЖКА','TECHNICAL SUPPORT','label.technical_support');
+insert into message_resource(kgz,rus,eng,messageKey) values ('КИРУУ','ВОЙТИ','LOGIN','label.login');
+insert into message_resource(kgz,rus,eng,messageKey) values ('ЧЫГУУ','ВЫЙТИ','LOGOUT','label.logout');
+insert into message_resource(kgz,rus,eng,messageKey) values ('КАТТАЛУУ','РЕГИСТРАЦИЯ','SIGNUP','label.signup');
+insert into message_resource(kgz,rus,eng,messageKey) values ('Издоо','Искать','Search','label.search');
+insert into message_resource(kgz,rus,eng,messageKey) values ('Аракеттер','Действие','Actions','label.actions');
+insert into message_resource(kgz,rus,eng,messageKey) values ('Жоор беруу учун юрист катары кириниз','Войдите как юрист чтобы отвечать','Login as lawyer to answer','label.login_as_lawyer');
+insert into message_resource(kgz,rus,eng,messageKey) values ('Кируу','Войти с','Connect with','label.connect_with');
+insert into message_resource(kgz,rus,eng,messageKey) values ('Жиберуу','Отправить','Submit','label.submit');
+insert into message_resource(kgz,rus,eng,messageKey) values ('Жиберуу','Отправить','Submit','label.male');
+insert into message_resource(kgz,rus,eng,messageKey) values ('Жиберуу','Отправить','Submit','label.female');
+insert into message_resource(kgz,rus,eng,messageKey) values ('Жиберуу','Отправить','Submit','label.close');
+insert into message_resource(kgz,rus,eng,messageKey) values ('Жиберуу','Отправить','Submit','label.edit');
+insert into message_resource(kgz,rus,eng,messageKey) values ('Жиберуу','Отправить','Submit','label.delete');
+insert into message_resource(kgz,rus,eng,messageKey) values ('Жиберуу','Отправить','Submit','label.leave_review');
+
+-- User
+insert into message_resource(kgz,rus,eng,messageKey) values ('Жиберуу','Отправить','Username','label.username');
+insert into message_resource(kgz,rus,eng,messageKey) values ('Жиберуу','Отправить','Username','label.remember');
+insert into message_resource(kgz,rus,eng,messageKey) values ('Жиберуу','Отправить','Password','label.password');
+insert into message_resource(kgz,rus,eng,messageKey) values ('Жиберуу','Отправить','Password','label.change_password');
+insert into message_resource(kgz,rus,eng,messageKey) values ('Жиберуу','Отправить','Password','label.confirm_password');
+insert into message_resource(kgz,rus,eng,messageKey) values ('Жиберуу','Отправить','Password','label.email');
+insert into message_resource(kgz,rus,eng,messageKey) values ('Жиберуу','Отправить','Password','label.name');
+insert into message_resource(kgz,rus,eng,messageKey) values ('Жиберуу','Отправить','Password','label.surname');
+insert into message_resource(kgz,rus,eng,messageKey) values ('Жиберуу','Отправить','Password','label.phone');
+insert into message_resource(kgz,rus,eng,messageKey) values ('Жиберуу','Отправить','Password','label.profile');
+
+-- Answer
+insert into message_resource(kgz,rus,eng,messageKey) values ('Жооп беруу','Добавить ответ','Add answer','label.add.answer');
+insert into message_resource(kgz,rus,eng,messageKey) values ('Жооптор','Ответы','Answers','label.answers');
+
+-- Category
+insert into message_resource(kgz,rus,eng,messageKey) values ('Категориялар','Категории','Categories','label.categories');
+insert into message_resource(kgz,rus,eng,messageKey) values ('Категориялар тизмеси','Список категорий','List of categories','label.category_list');
+insert into message_resource(kgz,rus,eng,messageKey) values ('Категория кошуу','Добавить категорию','Add category','label.add.category');
+
+-- Question
+insert into message_resource(kgz,rus,eng,messageKey) values ('Суроо суроо','Спросить вопрос','Ask question','label.ask.question');
+insert into message_resource(kgz,rus,eng,messageKey) values ('Суроолор','Вопросы','Questions','label.questions');
+insert into message_resource(kgz,rus,eng,messageKey) values ('Суроолор','Вопросы','Questions','label.question');
+insert into message_resource(kgz,rus,eng,messageKey) values ('Мазмуну','Содерж. вопроса','Question content','label.question.content');
+insert into message_resource(kgz,rus,eng,messageKey) values ('Коп суралган суроолор','Часто задаваемые вопросы','Most viewed questions','label.most.questions');
+
+-- Lawyers
+insert into message_resource(kgz,rus,eng,messageKey) values ('Юрист кошуу','Добавить юриста','Add lawyer','label.add.lawyer');
+insert into message_resource(kgz,rus,eng,messageKey) values ('Юристер','Юристы','Lawyers','label.lawyers');
+insert into message_resource(kgz,rus,eng,messageKey) values ('Юристер','Юристы','Lawyers','label.lawyer_info');
+insert into message_resource(kgz,rus,eng,messageKey) values ('Юристер','Юристы','Lawyers','label.top_lawyers');
+insert into message_resource(kgz,rus,eng,messageKey) values ('Юристер','Юристы','Lawyers','label.lawyer_profile');
+
+-- Translation
+insert into message_resource(kgz,rus,eng,messageKey) values ('Которулуштар','Переводы','Translations','label.translations');
+insert into message_resource(kgz,rus,eng,messageKey) values ('Которулуш кошуу','Добавить перевод','Add translation','label.add.translation');
+insert into message_resource(kgz,rus,eng,messageKey) values ('Башкы соз','Ключ. слово','Key word','label.key');
+
+-- Comment
+insert into message_resource(kgz,rus,eng,messageKey) values ('Башкы соз','Ключ. слово','Key word','label.comment');
+
+-- Technical support
+insert into message_resource(kgz,rus,eng,messageKey) values ('Башкы соз','Ключ. слово','Key word','label.add.technical');
